6af1741af023c3613ee9a8abcbf33899aafe93ac
zyfDog <1310956396@qq.com>
2022-03-02 21:43:35 +0800

    增加Netty客户端连接失败重试机制+改变Socket方式数据传输的端序，使其与Netty保持一致

--- rpc-core/src/main/java/com/zyf/rpc/netty/client/NettyClient.java
+++ rpc-core/src/main/java/com/zyf/rpc/netty/client/NettyClient.java
@@ -1,21 +1,18 @@
 package com.zyf.rpc.netty.client;
 
 import com.zyf.rpc.RpcClient;
-import com.zyf.rpc.codec.CommonDecoder;
-import com.zyf.rpc.codec.CommonEncoder;
 import com.zyf.rpc.entity.RpcRequest;
 import com.zyf.rpc.entity.RpcResponse;
 import com.zyf.rpc.enumeration.RpcError;
 import com.zyf.rpc.exception.RpcException;
 import com.zyf.rpc.serializer.CommonSerializer;
 import com.zyf.rpc.util.RpcMessageChecker;
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.Channel;
 import io.netty.util.AttributeKey;
 import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author zyf
@@ -29,18 +26,18 @@ public class NettyClient implements RpcClient {
 
     private String host;
     private int port;
-    private static final Bootstrap bootstrap;
+    // private static final Bootstrap bootstrap;
     private CommonSerializer serializer;
 
-    static {
+    /*static {
         EventLoopGroup group = new NioEventLoopGroup();
         bootstrap = new Bootstrap();
         bootstrap.group(group)
                 .channel(NioSocketChannel.class)
                 .option(ChannelOption.SO_KEEPALIVE, true);
-    }
+    }*/
 
-    public NettyClient(String host, int port){
+    public NettyClient(String host, int port) {
         this.host = host;
         this.port = port;
     }
@@ -51,7 +48,7 @@ public class NettyClient implements RpcClient {
             log.error("未设置序列化器");
             throw new RpcException(RpcError.SERIALIZER_NOT_FOUND);
         }
-        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+        /*bootstrap.handler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel ch) {
                 ChannelPipeline pipeline = ch.pipeline();
@@ -59,17 +56,21 @@ public class NettyClient implements RpcClient {
                         .addLast(new CommonEncoder(serializer))
                         .addLast(new NettyClientHandler());
             }
-        });
+        });*/
+        //保证自定义实体类变量的原子性和共享性的线程安全，此处应用于rpcResponse
+        AtomicReference<Object> result = new AtomicReference<>(null);
         try {
-            ChannelFuture future = bootstrap.connect(host, port).sync();
+            /*ChannelFuture future = bootstrap.connect(host, port).sync();
             log.info("客户端连接到服务端{}：{}", host, port);
             Channel channel = future.channel();
-            if(channel != null){
+            if(channel != null){*/
+            Channel channel = ChannelProvider.get(new InetSocketAddress(host, port), serializer);
+            if (channel.isActive()) {
                 //向服务端发请求，并设置监听，关于writeAndFlush()的具体实现可以参考：https://blog.csdn.net/qq_34436819/article/details/103937188
                 channel.writeAndFlush(rpcRequest).addListener(future1 -> {
-                    if(future1.isSuccess()){
+                    if (future1.isSuccess()) {
                         log.info(String.format("客户端发送消息：%s", rpcRequest.toString()));
-                    }else {
+                    } else {
                         log.error("发送消息时有错误发生:", future1.cause());
                     }
                 });
@@ -80,12 +81,15 @@ public class NettyClient implements RpcClient {
                 //get()阻塞获取value
                 RpcResponse rpcResponse = channel.attr(key).get();
                 RpcMessageChecker.check(rpcRequest, rpcResponse);
-                return rpcResponse.getData();
+                result.set(rpcResponse.getData());
+            } else {
+                //0表示”正常“退出程序，即如果当前程序还有在执行的任务，则等待所有任务执行完成以后再退出
+                System.exit(0);
             }
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             log.error("发送消息时有错误发生:", e);
         }
-        return null;
+        return result.get();
     }
 
     @Override
--- rpc-core/src/main/java/com/zyf/rpc/socket/util/ObjectWriter.java
+++ rpc-core/src/main/java/com/zyf/rpc/socket/util/ObjectWriter.java
@@ -35,11 +35,11 @@ public class ObjectWriter {
      * @description 将Int转换为字节数组
      */
     private static byte[] intToBytes(int value) {
-        byte[] des = new byte[4];
-        des[3] =  (byte) ((value>>24) & 0xFF);
-        des[2] =  (byte) ((value>>16) & 0xFF);
-        des[1] =  (byte) ((value>>8) & 0xFF);
-        des[0] =  (byte) (value & 0xFF);
-        return des;
+        byte[] src = new byte[4];
+        src[0] = (byte) ((value>>24) & 0xFF);
+        src[1] = (byte) ((value>>16) & 0xFF);
+        src[2] = (byte) ((value>>8) & 0xFF);
+        src[3] = (byte) (value & 0xFF);
+        return src;
     }
 }
